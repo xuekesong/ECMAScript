24d28cc6d9b085e15111e2a3e42400b555cd4c5c
xuekesong <15235592139@126.com>
2021-12-16 11:42:18 +0800

    箭头函数

---
+++ js/arrow_function.js
@@ -0,0 +1,10 @@
+let list = [1, 2, 3, 4, 5, 6, 7];
+let newList = list.map(function (item) {
+  return item * item;
+})
+
+console.log(newList);
+
+const arr = [1, 2, 3, 4, 5, 6, 7];
+const newArr = list.map(item => item * item);
+console.log(newArr);
